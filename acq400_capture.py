d39872b8e8b1efdea7522eab8acb644169197fb2
Peter Milne <peter.milne@d-tacq.com>
2018-04-28 12:34:13 +0100

    fix tabbing error 	modified:   acq400_capture.py

--- acq400_capture.py
+++ acq400_capture.py
@@ -4,12 +4,12 @@
     acq400_capture UUT1 [UUT2 ..]
     where UUT1 is the ip-address or host name of first uut
     example test client runs captures in a loop on one or more uuts
-    
+
     pre-requisite: UUT's are configured and ready to make a transient
     capture 
     eg clk is running. soft trg enabled
     eg transient length set.
-    
+
     loop continues "forever" until <CTRL-C>
 """
 
@@ -23,8 +23,8 @@ def run_shot(args):
     acq400_hapi.cleanup.init()
 
     for uut in uuts:
-	if args.transient != 'notouch':
-		uut.s0.transient = args.transient.replace(',', ' ')
+        if args.transient != 'notouch':
+            uut.s0.transient = args.transient.replace(',', ' ')
         if hasattr(uut.s0, 'TIM_CTRL_LOCK'):
             print "LOCKDOWN {}".format(uut)
             uut.s0.TIM_CTRL_LOCK = 1
@@ -34,7 +34,7 @@ def run_shot(args):
     try:
         shot_controller.run_shot(soft_trigger=args.soft_trigger)
         acq400_hapi.cleanup.sleep(1.0)            
-            
+
     except acq400_hapi.cleanup.ExitCommand:
         print("ExitCommand raised and caught")
     finally:
